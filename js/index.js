7c039113f91d3701c563e279900b7eb26f0c10dc
1sky <1192099172@qq.com>
2016-12-07 23:54:31 +0800

    上传

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function(){
+	$(".list-g").on("touchend",function(){
+		$(".list-g").css("background","#fff").css("border","0");
+		$(".title").css("color","#000");
+	})
+	
+//	var t=setInterval(function(){
+//		$(".header-left").css("left","20%");
+//		$(".header-right").css("right","20%")
+//	},300)
+})
